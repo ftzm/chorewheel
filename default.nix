35a32cfc26788d285202990d41c5b6b03013457d
ftzm <m@ftzm.org>
2022-01-25 22:19:05 +0100

    get nix compilation working again

--- default.nix
+++ default.nix
@@ -3,13 +3,20 @@ let
   compilerVersion = "ghc8107";
   compiler = pkgs.haskell.packages."${compilerVersion}";
 in
-  compiler.developPackage {
-    root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-          ghcid
-          pkgs.docker-compose
-          pkgs.postgresql
-        ]);
-  }
+ compiler.developPackage {
+   root = ./.;
+   overrides = with pkgs.haskell.lib; self: super: {
+     postgresql-syntax = dontCheck (markUnbroken (super.postgresql-syntax));
+     headed-megaparsec = dontCheck (markUnbroken (super.headed-megaparsec));
+   };
+   source-overrides = {
+      headed-megaparsec = "0.2.0.1";
+   };
+   modifier = drv:
+     pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+       [ cabal-install
+         ghcid
+         pkgs.docker-compose
+         pkgs.postgresql
+       ]);
+ }
--- migration/init.sql
+++ migration/init.sql
@@ -4,13 +4,42 @@ CREATE TABLE IF NOT EXISTS "user" (
   email TEXT UNIQUE NOT NULL
 );
 
+CREATE UNIQUE INDEX IF NOT EXISTS user_name_idx ON "user" (name);
+
 CREATE TABLE IF NOT EXISTS password (
-  user_id INT UNIQUE NOT NULL REFERENCES "user",
+  user_id INT PRIMARY KEY REFERENCES "user",
   password_hash text NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS refresh_token (
-  user_id INT UNIQUE NOT NULL REFERENCES "user",
-  token_string text UNIQUE NOT NULL,
+  user_id INT PRIMARY KEY REFERENCES "user",
+  token_string TEXT UNIQUE NOT NULL,
   expiry timestamp NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS refresh_token_token_string_idx
+  ON refresh_token (token_string);
+
+CREATE TABLE IF NOT EXISTS household (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS household_member (
+  household_id INT NOT NULL REFERENCES household,
+  user_id INT NOT NULL REFERENCES "user"
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS household_member_id
+  ON household_member (household_id, user_id);
+
+CREATE TABLE IF NOT EXISTS chore (
+  id SERIAL PRIMARY KEY,
+  household_id INT NOT NULL REFERENCES household,
+  name TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS chore_instance (
+  first_scheduled TIMESTAMPTZ,
+  completed_at TIMESTAMPTZ
 );
