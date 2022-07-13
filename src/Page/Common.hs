11a4a54201b3eb43a7ead7ad4508559912a2a7ab
ftzm <m@ftzm.org>
2022-07-13 21:31:12 +0200

    add sidebar

--- src/Page/Common.hs
+++ src/Page/Common.hs
@@ -3,7 +3,7 @@
 module Page.Common where
 
 import Lucid
-import Data.Text
+import qualified Data.Text as T
 
 import Page.Attribute
 
@@ -14,14 +14,22 @@ htmxScript =
       integrity_ "sha384-EzBXYPt0/T6gxNp0nuPtLkmRpmDBbjg6WmCUZRLXBBwYYmwAUxzlSGej0ARHX0Bo",
       crossOrigin_ "anonymous"
     ]
-    ("" :: Text)
+    ("" :: T.Text)
 
-container :: Text -> Html () -> Html ()
+container :: T.Text -> Html () -> Html ()
 container title body =
   html_ $ do
     head_ $ do
       title_ $ toHtml title
       --link_ [rel_ "stylesheet", type_ "text/css", href_ "screen.css"]
+      script_ [src_ "https://cdn.tailwindcss.com"] ("" :: T.Text)
+      script_ [src_ "https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"] ("" :: T.Text)
       htmxScript
       style_ "body{background:white}"
-    body_ body
+    body_ [class_ "flex"]$ do
+      nav_ [class_ "flex-initial h-screen p-4 bg-slate-300"] $ ul_ $ do
+        li_ $ a_ [href_ $ T.pack $ show $ linkURI $ _home rootLinks] "home"
+        li_ $ a_ [href_ $ T.pack $ show $ linkURI $ _households rootLinks] "households"
+        li_ $ a_ [href_ $ T.pack $ show $ linkURI $ _sessionLogout $ _session rootLinks] "logout"
+
+      div_ [class_ "flex-1 p-4"] body
