9960189384c0f55bd83b029c566f607bedf085b8
ftzm <m@ftzm.org>
2022-07-14 21:51:00 +0200

    simplify hot-reload script

--- watch.sh
+++ watch.sh
@@ -4,7 +4,25 @@ shopt -s globstar
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-./restart_ghci &
+(/usr/bin/env expect <<EOF
+set timeout -1
+# On any interrupt, reload and attempt to run the app
+trap {
+    send \003
+    send ":r\n"
+    send "import ChoreWheel\n"
+    send "runApp\n"
+} SIGINT
+spawn cabal repl
+expect Ok*
+send "import ChoreWheel\n"
+send "runApp\n"
+expect never
+EOF
+) &
+
 restart_pid=$!
 
-while sleep 0.1; do ls src/**/*.hs | entr -p -d kill -SIGINT $restart_pid; done
+while sleep 0.1; do
+    ls src/**/*.hs | entr -p -d kill -SIGINT $restart_pid;
+done
